9e23ed28abb36eb2bc0250d3cb2c15adf5b9162a
Michael <hazardu5@gmail.com>
2016-06-09 00:32:51 +0000

    Fixed bug where combat knife could not be used

--- cartridges/actual_cannibal/items/CombatKnife.js
+++ cartridges/actual_cannibal/items/CombatKnife.js
@@ -13,7 +13,7 @@ module.exports = function(quantity) {
         displayName: 'Combat Knife',
         quantity: quantity,
         description: "The combat knife is slightly rusted, but the blade still looks like it could do some damage.",
-        use: "You grasp the combat knife and slash it through the air a few times. It makes a cool swishing sound. You " +
-        "feel heroic. Sort of."
+        use: function(){return "You grasp the combat knife and slash it through the air a few times. It makes a cool swishing sound. You " +
+        "feel heroic. Sort of."}
     };
 };
